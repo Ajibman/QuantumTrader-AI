5c5949c39d5a95ec79324567a34f2ba8cba0bb17
Olagoke Ajibulu <ajibulurobotman@gmail.com>
2025-09-16 23:54:20 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "quantumtrader-ai",
+  "version": "1.0.0",
+  "type": "module",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "verify-ledger": "node server.js --verify",
+    "export-logs": "node server.js --export '{\"visitorId\":null}'"
+  },
+  "dependencies": {
+    "axios": "^1.4.0",
+    "cors": "^2.8.5",
+    "express": "^4.18.2",
+    "socket.io": "^4.8.0",
+    "body-parser": "^1.20.2"
+  }
+}
