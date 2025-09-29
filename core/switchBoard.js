ad23ea92eb58821edc218b9917a3d5be35b92cb8
Olagoke Ajibulu <ajibulurobotman@gmail.com>
2025-09-29 14:13:26 +0100

    Update switchBoard.js
    ```
    git Create switchboard.js in core folder: handles centralized control and activates wiring logic post-activation.
    ```
    
    ---

--- core/switchBoard.js
+++ core/switchBoard.js
@@ -1,1 +1,18 @@
----js
+```js
+// core/switchboard.js
+
+import { isActivated } from './activationTimer.js';
+import { initializeWiring } from '../logic/wiringHub.js';
+
+export function engageSwitchboard() {
+  if (isActivated()) {
+    console.log("QonexAI Switchboard: ACTIVATED");
+    initializeWiring();
+    // Add more control logic here as modules grow
+  } else {
+    console.log("QonexAI Switchboard: STANDBY – Awaiting Activation Time");
+  }
+}
+```
+
+---
