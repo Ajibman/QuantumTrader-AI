6c41528c80bb8fe361b54d092f81d097847ae387
Olagoke Ajibulu <ajibulurobotman@gmail.com>
2025-10-03 16:52:00 +0100

    Update commHub.js

--- core/commHub.js
+++ core/commHub.js
@@ -1,1 +1,12 @@
 ... js
+
+
+method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload)
+  });
+
+  const msg = await res.text();
+  document.getElementById("statusMsg").innerText = msg;
+});
+```
