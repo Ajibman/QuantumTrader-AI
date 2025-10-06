22b76f00a4dfdd2775def0bdc3ddbde9e9ab4ee1
Olagoke Ajibulu <ajibulurobotman@gmail.com>
2025-10-06 07:08:29 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -5,4 +5,22 @@ import TransmissionPreview from './components/TransmissionPreview';
 <TransmissionPreview />
 ```
 
+function checkGPSStatus() {
+  if (!navigator.geolocation) {
+    alert("Location services are required to use QonexAI.");
+    window.location.href = "/gps-required.html"; // Graceful block
+  } else {
+    navigator.geolocation.getCurrentPosition(
+      () => {}, 
+      () => {
+[10/6, 6:54 AM] ChatGPT 1-800-242-8478: alert("GPS is disabled. Please enable it to continue.");
+        window.location.href = "/gps-required.html";
+      }
+    );
+  }
+}
+
+window.onload = checkGPSStatus;
+```
+
 ---
