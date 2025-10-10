71a96fac6411df0006a09b8c8568ad978c9d8a43
Olagoke Ajibulu <ajibulurobotman@gmail.com>
2025-10-10 13:51:26 +0100

    Create missionControl.js

---
+++ core/assist/cpilot/missionControl.js
@@ -0,0 +1,22 @@
+```js
+
+✅ missionControl.js
+// core/assist/cpilot/missionControl.js
+
+function dispatchMission(event) {
+  switch (event.type) {
+    case 'shutdown':
+      console.log("MissionControl: Shutdown initiated.");
+      // Add more shutdown coordination if needed
+      break;
+    case 'alert':
+      console.log("MissionControl: Alert received ->", event.message);
+      break;
+    default:
+      console.log("MissionControl: Unknown event type.");
+  }
+}
+
+module.exports = { dispatchMission };
+```
+
