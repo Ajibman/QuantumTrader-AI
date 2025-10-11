78463d8fc2ef9f8e8250c5b9409cc015cfbc6f9e
Olagoke Ajibulu <ajibulurobotman@gmail.com>
2025-10-11 08:23:30 +0100

    Update flightManager.js

--- core/cpilot/flightManager.js
+++ core/cpilot/flightManager.js
@@ -1,1 +1,20 @@
-flightManager.js
+```js
+// core/cpilot/flightManager.js
+const { runMarketScan } = require('../missions/marketScan');
+const { sendHelpPrompt } = require('../missions/helpPrompt');
+
+function dispatchFlightPlan(user, request) {
+  switch (request.type) {
+    case "MARKET_SCAN":
+      return runMarketScan(user, request.payload);
+
+    case "HELP":
+      return sendHelpPrompt(user);
+
+    default:
+      return { status: "ignored", message: "Unknown flight type." };
+  }
+}
+
+module.exports = { dispatchFlightPlan };
+```
