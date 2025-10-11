407e6ee82e9def412fba8fd052d4b69738de4a7f
Olagoke Ajibulu <ajibulurobotman@gmail.com>
2025-10-11 08:24:55 +0100

    Update missionControl.js

--- core/cpilot/missionControl.js
+++ core/cpilot/missionControl.js
@@ -1,2 +1,13 @@
---js
-missionControl.js
+```js
+// core/cpilot/missionControl.js
+const { handleMission } = require('./cpilotCore');
+
+function triggerMission(user, missionRequest) {
+  const result = handleMission(user, missionRequest);
+  return result;
+}
+
+module.exports = { triggerMission };
+```
+
+
