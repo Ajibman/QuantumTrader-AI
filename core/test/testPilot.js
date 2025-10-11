57b33a1d135854438012ac64c01d4361c4a2d2c3
Olagoke Ajibulu <ajibulurobotman@gmail.com>
2025-10-11 13:57:20 +0100

    Update testPilot.js

--- core/test/testPilot.js
+++ core/test/testPilot.js
@@ -1,4 +1,3 @@
-
 ```js
 // test/testCPilot.js
 const { dispatchMission } = require('../core/cpilot/flightManager');
@@ -6,3 +5,17 @@ const mockUser = require('./mockUser'); // or use inline
 const mockMission = require('./mockMission');
 
 const result = dispatchMission(mockUser, mockMission);
+
+```
+✅ `test/testCPilot.js`
+
+const mockUser = require('./mockUser');
+const mockMission = require('./mockMission');
+const { dispatchMission } = require('../core/cpilot/flightManager');
+
+const result = dispatchMission(mockUser, mockMission);
+
+console.log("Dispatch Result:", result);
+```
+
+---
