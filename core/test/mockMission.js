b0e33b7f30e72c5844b0da8f9e13ca98bf0a98fa
Olagoke Ajibulu <ajibulurobotman@gmail.com>
2025-10-11 13:53:18 +0100

    Create mockMission.js

---
+++ core/test/mockMission.js
@@ -0,0 +1,13 @@
+```js
+✅ `test/mockMission.js`
+module.exports = {
+  id: 'mission001',
+  type: 'market-analysis',
+  parameters: {
+    sector: 'crypto',
+    timeframe: '24h'
+  },
+  requestedBy: 'user123',
+  urgency: 'high'
+};
+```
