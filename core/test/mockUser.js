9ffc2d5029061e44d84a3a31bb170db5b21f1de5
Olagoke Ajibulu <ajibulurobotman@gmail.com>
2025-10-11 13:54:59 +0100

    Create mockUser.js

---
+++ core/test/mockUser.js
@@ -0,0 +1,16 @@
+✅ `test/mockUser.js`
+```js
+module.exports = {
+  id: 'user123',
+  name: 'Test Pilot',
+  credentials: {
+    token: 'abc123xyz'
+  },
+  location: {
+    latitude: 6.5244,
+    longitude: 3.3792 // Lagos, Nigeria
+  }
+};
+```
+
+---
