22a62f685b4247d0f63c32aa48d61606feec4417
Olagoke Ajibulu <222703658+Ajibman@users.noreply.github.com>
2025-10-11 16:37:16 +0100

    Update missionControl.js

--- core/cpilot/missionControl.js
+++ core/cpilot/missionControl.js
@@ -10,4 +10,23 @@ function triggerMission(user, missionRequest) {
 module.exports = { triggerMission };
 ```
 
+// core/cpilot/missionControl.js
+const flightManager = require('./flightManager');
 
+function receiveMission(missionData) {
+  if (!missionData || !missionData.userId || !missionData.task) {
+    return { accepted: false, reason: "Invalid mission data." };
+  }
+
+  const missionId = generateMissionId();
+  const status = flightManager.assignMission({ ...missionData, missionId });
+
+  return { accepted: true, missionId, status };
+}
+
+function generateMissionId() {
+  return 'MSS-' + Math.random().toString(36).substring(2, 10).toUpperCase();
+}
+
+module.exports = { receiveMission };
+```
