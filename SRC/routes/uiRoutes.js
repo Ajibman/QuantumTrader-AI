a61b6f4993a8de1593254d75b15fab0cf853a581
Olagoke Ajibulu <222703658+Ajibman@users.noreply.github.com>
2025-10-12 08:22:49 +0100

    Create uiRoutes.js

---
+++ SRC/routes/uiRoutes.js
@@ -0,0 +1,28 @@
+'''js
+// routes/uiRoutes.js
+const express = require('express');
+const router = express.Router();
+const { processProfit } = require('../core/logic/traderLogic');
+
+// Simulated user profit + consent check
+router.post('/take-profit', (req, res) => {
+  const { profit, consent } = req.body;
+
+  if (typeof profit !== 'number' || typeof consent !== 'boolean') {
+    return res.status(400).json({ error: 'Invalid request format.' });
+  }
+
+  const result = processProfit(profit, consent);
+  res.json(result);
+});
+
+router.get('/social-responsibility', (req, res) => {
+  res.send('Welcome to QonexAI Social Responsibility Portal.');
+});
+
+router.get('/cooperatives', (req, res) => {
+  res.send('Welcome to QonexAI Cooperatives Network.');
+});
+
+module.exports = router;
+```
