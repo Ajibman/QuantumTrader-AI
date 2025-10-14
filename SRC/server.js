5a466e1a89c5110cd70e2e7cfffdbc9999d387cf
Olagoke Ajibulu <222703658+Ajibman@users.noreply.github.com>
2025-10-14 18:35:10 +0100

    Update server.js

--- SRC/server.js
+++ SRC/server.js
@@ -44,7 +44,7 @@ tradeEmitter.on('profitTaken', async ({ userId, profit }) => {
 {profit} taken by User ${userId}`);
   await handleProfitConsent(userId, profit); // calls logic to check consent & allocate
 });
-```
+
 
 const socialResponsocialResponsibilityRoutessibilityRoutes = require('./routes/socialResponsibility');
 app.use('/api/social-responsibility', socialResponsibilityRoutes);
