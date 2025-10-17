0b84d07e37a72bf5c0926572093ead1ef5f47240
Olagoke Ajibulu <222703658+Ajibman@users.noreply.github.com>
2025-10-17 13:40:29 +0100

    Update Node.js

--- src/Node.js
+++ src/Node.js
@@ -1,1 +1,10 @@
-node.js
+  '''js
+const path = require('path');
+const collab = require('./collab/index.js'); // Link to collaboration core
+
+if (!collab.status) {
+  console.error('[Node.js] Collaboration handshake failed. Cannot proceed.');
+  process.exit(1);
+}
+
+console.log('[Node.js] 🤝 Collaboration link active with QonexAI core modules.');
