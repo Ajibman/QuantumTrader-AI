d81880b21e6f3073bc22114f1c93eda7ca5942fc
Olagoke Ajibulu <222703658+Ajibman@users.noreply.github.com>
2026-01-02 17:36:09 +0100

    Add deepFreeze utility for immutable state
    git "Added a utility function to deep-freeze objects, ensuring immutability."
    git commit origin main

--- assets/js/core/utils/freeze.js
+++ assets/js/core/utils/freeze.js
@@ -1,1 +1,11 @@
-freeze.js
+// assets/js/core/utils/freeze.js
+// Utility to deep-freeze objects for immutable state
+
+export function deepFreeze(obj) {
+  Object.getOwnPropertyNames(obj).forEach((prop) => {
+    if (obj[prop] !== null && typeof obj[prop] === "object") {
+      deepFreeze(obj[prop]);
+    }
+  });
+  return Object.freeze(obj);
+}
