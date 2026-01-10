042a838a42e28c20155b19e30b83df2c777754be
Olagoke Ajibulu <222703658+Ajibman@users.noreply.github.com>
2026-01-10 19:50:03 +0100

    Update firebase.js

--- assets/js/core/firebase.js
+++ assets/js/core/firebase.js
@@ -1,3 +1,21 @@
 import { initFirebaseMirror } from './core/firebase.js';
 
 initFirebaseMirror(appState);
+
+export function initFirebaseMirror(state) {
+  const db = firebase.firestore(); // or firebase.database()
+
+  // Listen to changes from Firebase
+  db.collection('nonSensitiveState').onSnapshot(snapshot => {
+    snapshot.docChanges().forEach(change => {
+      if (change.type === 'added' || change.type === 'modified') {
+        state.updateFromMirror(change.doc.data());
+      }
+    });
+  });
+
+  // Mirror local changes to Firebase
+  state.onChange(newState => {
+    db.collection('nonSensitiveState').doc('mirrorDoc').set(newState);
+  });
+}
