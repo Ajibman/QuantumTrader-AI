71f1b55c51af36309f2a1fd122f2e36b798413a3
Olagoke Ajibulu <222703658+Ajibman@users.noreply.github.com>
2026-01-29 17:39:37 +0100

    Update cpilot_simulation_bind.js

--- core/js/cpilot_simulation_bind.js
+++ core/js/cpilot_simulation_bind.js
@@ -1,6 +1,7 @@
- // cpilot_simulation_bind.js
+ // core/cpilot/cpilot_simulation_bind.js
 
-import { CPilotEngine } from "../cpilot/cpilot_engine.js";
+import { CPilotEngine } from "./cpilot_engine.js";
+import { simulationFeed } from "../simulation/simulation_feed.js";
 
 const CPilotSimulationBind = {
   activeSignal: null,
@@ -12,17 +13,21 @@ const CPilotSimulationBind = {
     }
 
     if (!signal.permission?.cpilotAllowed) {
-      console.warn("CPilot not permitted for this signal");
+      console.warn("CPilotSimulationBind.start: CPilot not permitted by signal");
       return;
     }
 
     this.activeSignal = signal;
 
-    CPilotEngine.start({
-      signal,
-      mode: signal.mode,
-      timing: signal.timing,
-      context: signal.context
+    // Load signal into CPilot
+    CPilotEngine.loadSignal(signal);
+
+    // Start CPilot core
+    CPilotEngine.start();
+
+    // Start simulation feed and forward ticks
+    simulationFeed.start(signal.timing, (tick) => {
+      CPilotEngine.onMarketTick(tick);
     });
   },
 
@@ -32,41 +37,10 @@ const CPilotSimulationBind = {
       return;
     }
 
-    CPilotEngine.stop();
-    this.activeSignal = null;
-  }
-};
-
-export default CPilotSimulationBind;
-
-// core/cpilot/cpilot_simulation_bind.js
-
-import { CPilotEngine } from "./cpilot_engine.js";
-import { simulationFeed } from "../simulation/simulation_feed.js";
-
-let activeSignal = null;
-
-const CPilotSimulationBind = {
-  start(signal) {
-    if (!signal?.permission?.cpilotAllowed) {
-      console.warn("CPilot not permitted by signal");
-      return;
-    }
-
-    activeSignal = signal;
-
-    CPilotEngine.loadSignal(signal);
-    CPilotEngine.start();
-
-    simulationFeed.start(signal.timing, tick => {
-      CPilotEngine.onMarketTick(tick);
-    });
-  },
-
-  stop() {
     simulationFeed.stop();
     CPilotEngine.stop();
-    activeSignal = null;
+
+    this.activeSignal = null;
   }
 };
 
