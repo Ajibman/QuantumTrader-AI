18a4bf1c4a022bb6d016bd2bc47a855016672a5a
Olagoke Ajibulu <222703658+Ajibman@users.noreply.github.com>
2026-01-29 01:00:58 +0100

    Create index.js

---
+++ core/js/index.js
@@ -0,0 +1,16 @@
+// Example state object
+const AppState = {
+  wallet: {
+    exists: true,
+    subscriptionValid: true
+  },
+  traderLab: {
+    completed: false
+  },
+  cPilot: {
+    qualified: false
+  },
+  tradingFloor: {
+    approved: false
+  }
+};
