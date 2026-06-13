15374a53f4333ffafeca3738894c23f0f9380157
Olagoke Ajibulu <222703658+Ajibman@users.noreply.github.com>
2026-06-13 04:59:11 +0100

    Update autoTrader.js

--- core/autoTrader.js
+++ core/autoTrader.js
@@ -1,1 +1,25 @@
-autoTrader 
+// core/js/autoTrader.js
+
+import signalEngine from "./signalEngine.js";
+import riskEngine from "./riskEngine.js";
+import decisionEngine from "./decisionEngine.js";
+
+const autoTrader = {
+
+  async runCycle() {
+
+    const signals = await signalEngine.scan();
+
+    const risk = riskEngine.evaluate(signals);
+
+    const decision = decisionEngine.makeDecision(
+      signals,
+      risk
+    );
+
+    return decision;
+  }
+
+};
+
+export default autoTrader;
